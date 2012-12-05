ad27d0371affcc24eaf4cea167a359477fa7a18b
kchen12 <kchen12@stanford.edu>
2012-12-04 22:18:00 -0800

    changed lights back to vector<>* and fixed occlusion code to create proper ray

--- raytracer/Material.cpp
+++ raytracer/Material.cpp
@@ -41,7 +41,8 @@ float max(float a, float b) {
     return b;
 }
 
-bool occluderExists(STVector3 rayToLightSource, std::vector<SceneObject*> sceneObjects) {
+bool occluderExists(STPoint3 pt, STVector3 pointToLightSource, std::vector<SceneObject*> sceneObjects) {
+    Ray rayToLightSource = Ray(pointToLightSource, pt, 0, pointToLightSource.Length());
     for(int o = 0; o < sceneObjects.size(); ++o) {
         SceneObject* sceneObjectPtr = sceneObjects.at(o);
         if(sceneObjectPtr->intersection(rayToLightSource)) {
@@ -51,34 +52,34 @@ bool occluderExists(STVector3 rayToLightSource, std::vector<SceneObject*> sceneO
     return false;
 }
 
-STColor3f Material::getColor(STPoint3 intersection, STVector3 normal, Camera camera, std::vector<PointLight> pLights, std::vector<DirectionalLight> dLights, std::vector<AmbientLight> aLights, std::vector<SceneObject*> sceneObjects)
+STColor3f Material::getColor(STPoint3 intersection, STVector3 normal, Camera camera, std::vector<PointLight>* pLights, std::vector<DirectionalLight>* dLights, std::vector<AmbientLight>* aLights, std::vector<SceneObject*> sceneObjects)
 {
     STColor3f ambientTerm = STColor3f(0.,0.,0.);
     STColor3f diffuseTerm = STColor3f(0.,0.,0.);
     STColor3f specularTerm = STColor3f(0.,0.,0.);
 
-    for(int l = 0; l < aLights.size(); l++) {
-        ambientTerm += aLights.at(l).getColor();
+    for(int l = 0; l < aLights->size(); l++) {
+        ambientTerm += aLights->at(l).getColor();
     }
     ambientTerm *= mAmbient;
     
-    for(int l = 0; l < dLights.size(); l++) {
-        STVector3 L = dLights.at(l).pointToLightVector();
+    for(int l = 0; l < dLights->size(); l++) {
+        STVector3 L = dLights->at(l).pointToLightVector();
         L.Normalize();
-        if(occluderExists(L, sceneObjects)) continue;
+        if(occluderExists(intersection, L, sceneObjects)) continue;
 
         STVector3 R = 2.0*STVector3::Dot(L,normal)*normal-L;
         R.Normalize();
         STVector3 V = (-1.0f)*camera.getW();
 
-        diffuseTerm += mDiffuse * dLights.at(l).getColor() * max(0., STVector3::Dot(L, normal));
+        diffuseTerm += mDiffuse * dLights->at(l).getColor() * max(0., STVector3::Dot(L, normal));
 
-        specularTerm += mSpecular * dLights.at(l).getColor() * pow(max(0., STVector3::Dot(R, V)), mShine);
+        specularTerm += mSpecular * dLights->at(l).getColor() * pow(max(0., STVector3::Dot(R, V)), mShine);
     }
     
-    for(int l = 0; l < pLights.size(); l++) {
-        STVector3 L = pLights.at(l).pointToLightVector(intersection);
-        if(occluderExists(L, sceneObjects)) continue;
+    for(int l = 0; l < pLights->size(); l++) {
+        STVector3 L = pLights->at(l).pointToLightVector(intersection);
+        if(occluderExists(intersection, L, sceneObjects)) continue;
         L.Normalize();
         
         STVector3 R = 2.0*STVector3::Dot(L,normal)*normal-L;        
@@ -86,9 +87,9 @@ STColor3f Material::getColor(STPoint3 intersection, STVector3 normal, Camera cam
         STVector3 V = camera.getEye() - intersection;
         V.Normalize();
         
-        diffuseTerm += mDiffuse * pLights.at(l).getColor() * max(0, STVector3::Dot(L, normal));
+        diffuseTerm += mDiffuse * pLights->at(l).getColor() * max(0, STVector3::Dot(L, normal));
         
-        specularTerm += mSpecular * pLights.at(l).getColor() * pow(max(0, STVector3::Dot(R, V)), mShine);
+        specularTerm += mSpecular * pLights->at(l).getColor() * pow(max(0, STVector3::Dot(R, V)), mShine);
     }
     
     return ambientTerm + diffuseTerm + specularTerm;
--- raytracer/Material.h
+++ raytracer/Material.h
@@ -39,7 +39,7 @@ public:
     STColor3f getSpecular() const { return mSpecular; }
     STColor3f getMirror()   const { return mMirror;   }
     
-    STColor3f getColor(STPoint3 intersection, STVector3 normal, Camera camera, std::vector<PointLight> pLights, std::vector<DirectionalLight> dLights, std::vector<AmbientLight> aLights, std::vector<SceneObject*> sceneObjects);
+    STColor3f getColor(STPoint3 intersection, STVector3 normal, Camera camera, std::vector<PointLight>* pLights, std::vector<DirectionalLight>* dLights, std::vector<AmbientLight>* aLights, std::vector<SceneObject*> sceneObjects);
 
     
 private:  
